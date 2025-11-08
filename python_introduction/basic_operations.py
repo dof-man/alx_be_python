79bb6005d580aafe23efb4762cf217ac43e6858f
dof-man <onucheojofriday@gmail.com>
2025-11-08 18:23:11 +0100

    basic operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+diff_result = number1 - number2
+product_result = number1 * number2 
+print(" Addition of 10 and 5 is", sum_result)
+print(" Subtraction of 10 and 5 is", diff_result)
+print(" Multiplication of 10 and 5 is", product_result)
